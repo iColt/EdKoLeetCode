eede62151a009de63e46a023c27a30156309b668
iColt <eduard.kobzik@gmail.com>
2025-03-23 23:01:13 +0100

    [2] Add two numbers - working

---
+++ LeetCode/LinkedLists.cs
@@ -0,0 +1,18 @@
+﻿using LeetCodeTasks.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.LeetCode;
+
+public class LinkedLists
+{
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        ListNode outputNode = new ListNode();
+
+        return outputNode;
+    }
+}
---
+++ LeetCodeTasks.Tests/LinkedListsFixture.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCodeTasks.Tests;
+
+[TestFixture]
+internal class LinkedListsFixture
+{
+    [TestCase(new int[] { 0, 3, 3, 0 }, new int[] { 0, 3 }, new int[] { 0, 3 })]
+    [TestCase(new int[] { -3, 4, 3, 90 }, new int[] { 0, 3 }, new int[] { 0, 2 })]
+    [TestCase(new int[] { 2, 7, 11, 15 }, new int[] { 0, 3 }, new int[] { 0, 1 })]
+    [TestCase(new int[] { 3, 2, 4 }, new int[] { 0, 3 }, new int[] { 1, 2 })]
+    [TestCase(new int[] { 3, 3 }, new int[] { 0, 3 }, new int[] { 0, 1 })]
+    public void Test_AddTwoNumbers(int[] first, int[] second, int[] output)
+    {
+
+    }
+}
