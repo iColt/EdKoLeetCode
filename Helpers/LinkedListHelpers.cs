7a75c609365702fbfff265c33e9ea952e7fbb75b
iColt <eduard.kobzik@gmail.com>
2025-05-14 20:33:33 +0200

    [2] Work in progress

---
+++ Helpers/LinkedListHelpers.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCodeTasks.Helpers;
+
+public static class LinkedListHelpers
+{
+    public static int[] ExposeLinkedList(this ListNode list, int maxLength, out int realLength)
+    {
+        int[] arr = new int[maxLength];
+        realLength = 0;
+        ListNode current = list;
+
+        while(true)
+        {
+            if(current == null)
+            {
+                return arr;
+            }
+
+            realLength++;
+            current = current.next;
+        }
+    }
+
+    public static ListNode CreateLinkedList(this int[] array)
+    {
+        ListNode node = new ListNode(array[0]);
+        ListNode currentNode = node;
+        for(int i = 1; i < array.Length; i++)
+        {
+            if(i  == array.Length - 1)
+            {
+                return node;
+            }
+
+            ListNode newNode = new ListNode(array[i]);
+            currentNode.next = newNode;
+            currentNode = newNode;
+        }
+
+        return node;
+    }
+}
--- LeetCode/LinkedLists.cs
+++ LeetCode/LinkedLists.cs
@@ -1,18 +1,85 @@
 ﻿using LeetCodeTasks.Helpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LeetCodeTasks.LeetCode;
 
 public class LinkedLists
 {
+    // #2 - Add Two Numbers
     public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
         ListNode outputNode = new ListNode();
+        int lengthFirst;
+        int[] first = LinkedListHelpers.ExposeLinkedList(l1, 100, out lengthFirst);
+        int lengthSecond;
+        int[] second = LinkedListHelpers.ExposeLinkedList(l2, 100, out lengthSecond);
+        
+        Array.Resize<int>(ref first, lengthFirst);
+        Array.Resize<int>(ref second, lengthSecond);
+
+        Array.Reverse(first);
+        Array.Reverse(second);
+
+        int[] resultArray;
+        if(lengthFirst > lengthSecond)
+        {
+            resultArray = SumTwoArraysByNum(first, lengthFirst, second, lengthSecond);
+        }
+        else
+        {
+            resultArray = SumTwoArraysByNum(second, lengthSecond, first, lengthFirst);
+        }
+
+        Array.Reverse(resultArray);
+
+        outputNode = resultArray.CreateLinkedList();
+        // Approach 1:
+        // reverse first, calculate length
+        // reverse second, calculate length
+        // join
+
+        // Approach 2
+        // run reccursion on both two linked lists
+        // reccursion should return integer 0 < x < 19
+        // when reccursion meet the end of any list -  
 
         return outputNode;
+    }
+
+    private static int[] SumTwoArraysByNum(int[] first, int lengthFirst, int[] second, int lengthSecond)
+    {
+        int[] resultArray = new int[lengthFirst + 1];
+        int increaseValue = 0;
+
+        for (int i = 0; i < lengthFirst; i++)
+        {
+            if(i < lengthSecond - 1)
+            {
+                int sum = first[i] + second[i] + increaseValue;
+                if(sum > 9)
+                {
+                    increaseValue = sum % 10;
+                    resultArray[i] = sum / 10;
+                }
+                else
+                {
+                    resultArray[i] = sum;
+                }
+            }
+            else
+            {
+                int sum = first[i] + increaseValue;
+                if (sum > 9)
+                {
+                    increaseValue = sum % 10;
+                    resultArray[i] = sum / 10;
+                }
+                else
+                {
+                    resultArray[i] = sum;
+                }
+            }
+        }
+
+        return resultArray;
     }
 }
