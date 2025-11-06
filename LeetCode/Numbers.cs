535f4ffc5b6195fd1689af33cf85f91c1dab523e
iColt <eduard.kobzik@gmail.com>
2025-11-06 22:08:00 +0100

    [9] Solved non optimal

---
+++ LeetCode/Numbers.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeTasks.LeetCode;
+
+public static class Numbers
+{
+    // #9 - IsPalindrome - medium memory and runtime performance
+    public static  bool IsPalindrome(int x)
+    {
+        if(x >= 0 && x < 10) return true;
+        if(x < 0) return false;
+
+        int numberLength = 0;
+        //x < 2^31
+        int[] arrNumber = new int[10];
+        while(true)
+        {
+            arrNumber[numberLength] = x % 10;
+            x = x / 10;
+            numberLength++;
+
+            if(x == 0)
+            {
+                break;
+            }
+        }
+
+        Array.Resize(ref arrNumber, numberLength);
+
+        for(int i = 0; i < numberLength / 2; i++)
+        {
+            if (arrNumber[i] != arrNumber[numberLength - i - 1])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ LeetCodeTasks.Tests/NumbersFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCodeTasks.Tests;
+[TestFixture]
+internal class NumbersFixture
+{
+    [TestCase(121, true)]
+    [TestCase(-121, false)]
+    [TestCase(10, false)]
+    [TestCase(123454321, true)]
+    [TestCase(1234554321, true)]
+    public static void Test_IsPalindrome(int number, bool result)
+    {
+        Assert.That(Numbers.IsPalindrome(number), Is.EqualTo(result));
+    }
+}
