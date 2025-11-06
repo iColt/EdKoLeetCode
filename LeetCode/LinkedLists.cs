c208f3550daa71104583eee396a94523a1f473ab
iColt <eduard.kobzik@gmail.com>
2025-11-06 21:51:22 +0100

    Solve #2 in non-optimal way

--- LeetCode/LinkedLists.cs
+++ LeetCode/LinkedLists.cs
@@ -4,7 +4,7 @@ namespace LeetCodeTasks.LeetCode;
 
 public class LinkedLists
 {
-    // #2 - Add Two Numbers
+    // #2 - Add Two Numbers - non optimal in runtime, non optimal in memory
     public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
         ListNode outputNode = new ListNode();
@@ -19,82 +19,30 @@ public class LinkedLists
         Array.Reverse(first);
         Array.Reverse(second);
 
-        int[] resultArray;
-        // just convert to classic long numbers and sum?
-        // (maybe it will be not correct and I need to save current approach just in case)
-        if (lengthFirst > lengthSecond)
-        {
-            resultArray = SumTwoArraysByNum(first, lengthFirst, second, lengthSecond);
-        }
-        else
-        {
-            resultArray = SumTwoArraysByNum(second, lengthSecond, first, lengthFirst);
-        }
-
-        Array.Reverse(resultArray);
-
+        int[] resultArray = AddDigitArrays(first, second);
         outputNode = resultArray.CreateLinkedList();
-        // Approach 1:
-        // reverse first, calculate length
-        // reverse second, calculate length
-        // join
-
-        // Approach 2
-        // run reccursion on both two linked lists
-        // reccursion should return integer 0 < x < 19
-        // when reccursion meet the end of any list -  
 
         return outputNode;
     }
 
-    // no initial revert = incorrect numbers
-    //we should convert number to array and just sum it, then write to output array or event node
-    private static int[] SumTwoArraysByNum(int[] first, int lengthFirst, int[] second, int lengthSecond)
+    static int[] AddDigitArrays(int[] a, int[] b)
     {
-        int[] resultArray = new int[lengthFirst + 1];
-        int increaseValue = 0;
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+        var result = new List<int>(Math.Max(a.Length, b.Length) + 1);
 
-        for (int i = 0; i < lengthFirst; i++)
+        while (i >= 0 || j >= 0 || carry > 0)
         {
-            if(i < lengthSecond)
-            {
-                int sum = first[i] + second[i] + increaseValue;
-                if(sum > 9)
-                {
-                    increaseValue = sum / 10;
-                    resultArray[i] = sum % 10;
-                }
-                else
-                {
-                    resultArray[i] = sum;
-                    increaseValue = 0;
-                }
-            }
-            else
-            {
-                int sum = first[i] + increaseValue;
-                if (sum > 9)
-                {
-                    increaseValue = sum % 10;
-                    resultArray[i] = sum / 10;
-                }
-                else
-                {
-                    resultArray[i] = sum;
-                    increaseValue = 0;
-                }
-            }
+            int digitA = (i >= 0) ? a[i--] : 0;
+            int digitB = (j >= 0) ? b[j--] : 0;
+
+            int sum = digitA + digitB + carry;
+            result.Add(sum % 10);
+            carry = sum / 10;
         }
 
-        if(increaseValue != 0)
-        {
-            resultArray[lengthFirst + 1] = increaseValue;
-        }
-        else
-        {
-            Array.Resize(ref resultArray, lengthFirst);
-        }
-
-        return resultArray;
+        result.Reverse();
+        return result.ToArray();
     }
 }
