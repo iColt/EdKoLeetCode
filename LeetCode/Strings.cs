85e12af4fb2ae6bf012641882ea5f10867512b26
iColt <eduard.kobzik@gmail.com>
2025-11-08 14:23:39 +0100

    [71] - Preparation

---
+++ LeetCode/Strings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeTasks.LeetCode;
+
+public static class Strings
+{
+    #region 71 - Simplify path
+
+    public static string SimplifyPath(string path)
+    {
+        return string.Empty;
+    }
+
+    #endregion
+}
---
+++ LeetCodeTasks.Tests/StringsFixture.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCodeTasks.Tests;
+
+[TestFixture]
+public sealed class StringsFixture
+{
+    public void Test_SimplifyPath(string path, string result)
+    {
+        Assert.That(Strings.SimplifyPath(path), Is.EqualTo(result));
+    }
+}
