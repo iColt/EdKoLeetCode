dfc5532068844644509adb107aeb96312c441533
iColt <eduard.kobzik@gmail.com>
2025-11-18 19:24:28 +0100

    [15] Solved

--- LeetCode/Arrays.cs
+++ LeetCode/Arrays.cs
@@ -118,9 +118,55 @@ public class Arrays
 
     #endregion
 
-    #region 15 3Sum
+    #region 15 3Sum - 45/51 Medium
 
     public static IList<IList<int>> ThreeSum(int[] nums)
+    {
+        Array.Sort(nums);
+        var result = new List<IList<int>>();
+
+        for (int i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0)
+                {
+                    result.Add(new List<int> { nums[i], nums[left], nums[right] });
+
+                    // skip duplicates
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+
+                    left++;
+                    right--;
+                }
+                else if (sum < 0)
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    #region Old
+
+    public static IList<IList<int>> ThreeSum2(int[] nums)
     {
         List<IList<int>> result = new List<IList<int>>();
 
@@ -221,6 +267,8 @@ public class Arrays
         }
         return result;
     }
+
+    #endregion
 
     #endregion
 
