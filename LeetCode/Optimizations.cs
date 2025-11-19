6b1c68c287b6699ffe593b029e5f9f8da405e355
iColt <eduard.kobzik@gmail.com>
2025-11-19 20:52:59 +0100

    [121] WIP

---
+++ LeetCode/Optimizations.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeTasks.LeetCode;
+
+public static class Optimizations
+{
+    #region 121 - Best Time to Buy and Sell Stock
+
+    public static int MaxProfit(int[] prices)
+    {
+        Dictionary<int, int> dayValueArray = prices.Select((value, index) => new { value, index }).OrderByDescending(x => x.value)
+                      .ToDictionary(pair => pair.index, pair => pair.value);
+
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.Length - 1; i++)
+        {
+            
+        }
+        return maxProfit;
+    }
+
+    private static int MaxProfitForBuyDay(Dictionary<int, int> dayValueArray, int buyIndex)
+    {
+        int index = -1;
+
+        return index;
+    }
+
+    #endregion
+}
---
+++ LeetCodeTasks.Tests/LeetCode/OptimizationsFixture.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCodeTasks.Tests.LeetCode;
+[TestFixture]
+public sealed class OptimizationsFixture
+{
+    [TestCase(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
+    [TestCase(new int[] { 7, 6, 4, 3, 1 }, 0)]
+    public void Test_MaxProfit(int[] prices, int output)
+    {
+        Assert.That(Optimizations.MaxProfit(prices), Is.EqualTo(output));
+    }
+}
