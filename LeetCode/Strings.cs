f65eddfe37dc324371af679c96cccd74068f5849
iColt <eduard.kobzik@gmail.com>
2025-12-01 20:27:03 +0100

    [13] Solved [Optimal]

--- LeetCode/Strings.cs
+++ LeetCode/Strings.cs
@@ -53,9 +53,9 @@ public static class Strings
 
     #endregion
 
-    #region 13 Roman to Integer - 9/14 - poor performace
+    #region 13 Roman to Integer - 9/14 - poor performace || 71.5/74 runtume/memory with one loop and +- approach
 
-    public static int RomanToInt(string s)
+    public static int RomanToIntOld(string s)
     {
         int number = 0;
 
@@ -107,6 +107,38 @@ public static class Strings
         }
 
         return number;
+    }
+
+    public static int RomanToInt(string s)
+    {
+        var map = new Dictionary<char, int>
+        {
+            ['I'] = 1,
+            ['V'] = 5,
+            ['X'] = 10,
+            ['L'] = 50,
+            ['C'] = 100,
+            ['D'] = 500,
+            ['M'] = 1000
+        };
+
+        int total = 0;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            int value = map[s[i]];
+
+            if (i + 1 < s.Length && map[s[i + 1]] > value)
+            {
+                total -= value;
+            }
+            else
+            {
+                total += value;
+            }
+        }
+
+        return total;
     }
 
     #endregion
